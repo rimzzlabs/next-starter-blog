6970f845988e784a567cc019619418e721b8c72e
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-04-12 23:50:26 +0700

    style: modify lint staged config

---
+++ .lintstagedrc.js
@@ -0,0 +1,8 @@
+const path = require('path')
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`
+
+module.exports = {
+  './src/*.{js,jsx,ts,tsx}': [buildEslintCommand]
+}
