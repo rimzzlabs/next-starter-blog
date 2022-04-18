815237267c2d32341c92b0f0609e9cbcfdbaf66d
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-04-19 01:24:53 +0700

    feat: change libs from react to preact in prod
    assign object library from react to preact on production

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  // change library from react to preact when in production
+  webpack(config, { dev, isServer }) {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat'
+      })
+      return config
+    }
+  }
 }
 
 module.exports = nextConfig
