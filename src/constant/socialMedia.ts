ed8efd055086eca3904a3a6ef08ed9040d9917d5
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-04-18 23:12:33 +0700

    feat: add social media constant

---
+++ src/constant/socialMedia.ts
@@ -0,0 +1,19 @@
+interface SocialMediaProps {
+  href: string
+  platform: string
+}
+
+export const socialMedia: Array<SocialMediaProps> = [
+  {
+    platform: 'Instagram',
+    href: 'https://instagram.com/john-doe'
+  },
+  {
+    platform: 'Twitter',
+    href: 'https://twitter.com/john-doe'
+  },
+  {
+    platform: 'Linkedin',
+    href: 'https://linkedin.com/in/john-doe'
+  }
+]
