8c36168e557a245d09988c9c8ed2a3be788c0143
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-04-18 23:15:37 +0700

    feat: create Hero component

---
+++ src/components/template/Hero.tsx
@@ -0,0 +1,17 @@
+export interface HeroProps {
+  title: string
+  description: string
+  children?: React.ReactNode
+}
+
+const Hero: React.FunctionComponent<HeroProps> = ({ title, description, children }) => {
+  return (
+    <section className='pb-20 pt-10'>
+      <h1 className='md:text-5xl'>{title}</h1>
+      <p className='max-w-prose'>{description}</p>
+      {children}
+    </section>
+  )
+}
+
+export default Hero
