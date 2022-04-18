e38634725bafa694f880ddce143af6bc829209a7
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-04-18 23:08:38 +0700

    style: add primary color (lime) and secondary color (amber)

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,20 +14,27 @@ module.exports = {
       colors: {
         main: {
           1: colors.zinc[50],
-          2: colors.zinc[200],
+          1.5: colors.zinc[200],
+          2: colors.zinc[400],
           3: colors.zinc[700],
           4: colors.zinc[800],
           5: colors.zinc[900]
         },
         primary: {
-          1: colors.indigo[300],
-          2: colors.indigo[400],
-          3: colors.indigo[500],
-          4: colors.indigo[600],
-          5: colors.indigo[700]
+          0.5: colors.lime[100],
+          1: colors.lime[300],
+          2: colors.lime[400],
+          3: colors.lime[500],
+          4: colors.lime[600],
+          5: colors.lime[700]
+        },
+        secondary: {
+          1: colors.amber[300],
+          2: colors.amber[400],
+          3: colors.amber[500]
         }
       }
     }
   },
-  plugins: []
+  plugins: [require('@tailwindcss/typography')]
 }
