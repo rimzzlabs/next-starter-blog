d077c8e52a0b13087a14e5b1f618531cf4c8fa0a
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-04-19 01:47:43 +0700

    fix: unable to switch page transition
    remove preact and change next.config file to default

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  // change library from react to preact when in production
-  webpack(config, { dev, isServer }) {
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat'
-      })
-    }
-    return config
-  }
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
