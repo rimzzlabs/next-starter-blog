1b482d7da1849e3b7920b49ca7fa3bddac8658bc
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-04-19 04:15:04 +0700

    feat: add helpers to parse mdx file

---
+++ src/helpers/getBlog.ts
@@ -0,0 +1,60 @@
+import { BlogProps } from '@/data/blog/blog.type'
+
+import getPathDirectory, { CONTENT_DIRECTORY } from './getPathDirectory'
+
+import fs from 'fs'
+import matter from 'gray-matter'
+import path from 'path'
+
+/**
+ * It takes a slug as a parameter, and returns an object with the data and content of the blog post.
+ * The data is the frontmatter of the blog post, and the content is the body of the blog post.
+ * The data is typed as BlogProps, which is an interface that I created in the `@/data/blog/blog.types.ts` file.
+
+ * The function uses the slug to find the blog post, and then uses the matter function to parse the
+ * frontmatter and body of the blog post.
+ *
+ * The function returns an object with the data and content of the blog post.
+ * @param slug - the slug of the blog post
+ * @returns The data and content of the blog post.
+ */
+export const getBlogBySlug = (slug: string) => {
+  const PATH_DIR = path.join(`${CONTENT_DIRECTORY}/blog`, `${slug}.mdx`)
+  const file = fs.readFileSync(PATH_DIR, 'utf8')
+
+  const { data, content } = matter(file)
+
+  return {
+    data: data as BlogProps,
+    content
+  }
+}
+
+/**
+ * It takes the path of the blog directory, gets all the files in that directory, reads the file, and
+ * returns an object with the data, slug, and content of the file.
+ *
+ * The data is the frontmatter of the file, the slug is the name of the file, and the content is the
+ * content of the file.
+ *
+ * The data is typed as BlogProps, which we can see at '@/data/blog/blog.type.ts'
+ * @returns An array of objects.
+ */
+const getBlog = () => {
+  const paths = getPathDirectory('/blog')
+
+  return paths.map((p) => {
+    const PATH_DIR = path.join(`${CONTENT_DIRECTORY}/blog`, p)
+    const file = fs.readFileSync(PATH_DIR, 'utf8')
+
+    const { data, content } = matter(file)
+
+    return {
+      data: data as BlogProps,
+      slug: p.replace('.mdx', ''),
+      content
+    }
+  })
+}
+
+export default getBlog
