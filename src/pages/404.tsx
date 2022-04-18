44ac171d5ab3fe58e5cdab9fbf53768c9e12a72d
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-04-18 23:42:00 +0700

    feat: add page transition with framer motion

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,19 +4,26 @@ import clsx from 'clsx'
 import type { NextPage } from 'next'
 import Link from 'next/link'
 
-const NotFoundPage: NextPage = () => (
-  <Layout title='404 Error' template='Page Not Found' description='The page you are looking for, are not found'>
-    <section className={clsx('flex flex-col items-center justify-center', '-mt-16 md:-mt-20', 'min-h-screen')}>
-      <h1 className='text-center'>404 | Not Found</h1>
-      <p className='text-center max-w-prose mt-4'>
-        The page you are looking for, are not found, if you find this was a mistake, please contact us at{' '}
-        <em>company@mail.cs</em>
-      </p>
-      <Link href='/'>
-        <a className={clsx('mt-4', 'text-primary-3 dark:text-primary-2')}>Back to Home</a>
-      </Link>
-    </section>
-  </Layout>
-)
+const NotFoundPage: NextPage = () => {
+  const meta = {
+    title: '404 Error',
+    template: 'Page Not Found',
+    description: 'The page you are looking for, are not found'
+  }
+  return (
+    <Layout {...meta}>
+      <section className={clsx('flex flex-col items-center justify-center', '-mt-16 md:-mt-20', 'min-h-screen')}>
+        <h1 className='text-center'>404 | Not Found</h1>
+        <p className='text-center max-w-prose mt-4'>
+          The page you are looking for, are not found, if you find this was a mistake, please contact us at{' '}
+          <em>company@mail.cs</em>
+        </p>
+        <Link href='/'>
+          <a className={clsx('mt-4', 'text-primary-3 dark:text-primary-2')}>Back to Home</a>
+        </Link>
+      </section>
+    </Layout>
+  )
+}
 
 export default NotFoundPage
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,48 @@ import '@/styles/tailwind.css'
 
 import Header from '@/components/organism/Header'
 
+import { AnimatePresence, LazyMotion, Variants, domAnimation, m } from 'framer-motion'
 import { ThemeProvider } from 'next-themes'
 import type { AppProps } from 'next/app'
+import { Fragment } from 'react'
 
-function MyApp({ Component, pageProps }: AppProps) {
+const v: Variants = {
+  hidden: {
+    opacity: 0,
+    y: 20
+  },
+  visible: {
+    opacity: 1,
+    y: 0
+  },
+  exit: {
+    opacity: 0,
+    y: 0
+  }
+}
+
+function MyApp({ Component, pageProps, router }: AppProps) {
   return (
     <ThemeProvider attribute='class' storageKey='theme' enableSystem>
       <Header />
-      <Component {...pageProps} />
+      <LazyMotion features={domAnimation}>
+        {/* disable initial animation to get higher score on lighthouse */}
+        <AnimatePresence initial={false} onExitComplete={() => window.scrollTo(0, 0)} exitBeforeEnter>
+          <m.div
+            key={router.route}
+            variants={v}
+            initial='hidden'
+            animate='visible'
+            exit='exit'
+            transition={{
+              duration: 0.25,
+              type: 'tween'
+            }}
+          >
+            <Component as={Fragment} {...pageProps} />
+          </m.div>
+        </AnimatePresence>
+      </LazyMotion>
     </ThemeProvider>
   )
 }
