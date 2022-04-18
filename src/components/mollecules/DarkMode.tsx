cffc8bb3656e3f012f4cc3a91e5c4b77711265be
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-04-19 06:31:59 +0700

    style: add ring on hover

--- src/components/mollecules/DarkMode.tsx
+++ src/components/mollecules/DarkMode.tsx
@@ -11,7 +11,13 @@ const DarkMode = () => {
   return (
     <button
       onClick={changeTheme}
-      className={clsx('inline-flex items-center justify-center', 'w-9 h-9 rounded-lg', 'bg-main-1.5 dark:bg-main-4')}
+      className={clsx(
+        'inline-flex items-center justify-center',
+        'w-9 h-9 rounded-lg transition-all hover:ring',
+        'bg-main-1.5 dark:bg-main-4',
+        'ring-main-2 dark:ring-main-3',
+        'ring-offset-2 ring-offset-white dark:ring-offset-main-5'
+      )}
     >
       {theme === 'dark' ? (
         <SunIcon className='w-4 md:w-5 h-4 md:h-5 text-yellow-500' />
