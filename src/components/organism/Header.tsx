1b899ad3c2953adbffecb2de54649faaebf02e95
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-04-19 21:24:00 +0700

    style: add z index

--- src/components/organism/Header.tsx
+++ src/components/organism/Header.tsx
@@ -11,7 +11,7 @@ const Header = () => {
   if (pathname === '/_error' || pathname === '/404') return null
 
   return (
-    <header className={clsx('fixed inset-0 h-20', 'bg-main-1 dark:bg-main-5')}>
+    <header className={clsx('fixed inset-0 h-20', 'bg-main-1 dark:bg-main-5 z-50')}>
       <div className={clsx('h-2 w-full', 'bg-gradient-to-r', 'from-primary-3 to-secondary-3')} />
 
       <section className={clsx('layout', 'flex items-center justify-between', 'h-full -mt-1.5')}>
