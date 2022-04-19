ad6c23661e964fea79a3acd64232f9207bcbc4f9
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-04-19 21:26:16 +0700

    feat: add more features
    Searchbar, Latest Post, and All Kind Of Post

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,19 +1,58 @@
+import Searchbar from '@/components/mollecules/Searchbar'
+import BlogList from '@/components/organism/BlogList'
 import Hero from '@/components/template/Hero'
 import Layout from '@/components/template/Layout'
 
-import type { NextPage } from 'next'
+import { BlogProps } from '@/data/blog/blog.type'
+import getBlog from '@/helpers/getBlog'
+import useSearchBlogQuery from '@/hooks/useSearchBlogQuery'
 
-const BlogPage: NextPage = () => {
-  const meta = {
-    title: 'Blog',
-    description: `I've been writing online since 2020, mostly about data science, machine learning and tech careers. On purpose for documentation while able to share my knowledge. Use the search below to filter by title.`
-  }
+import type { GetStaticProps, NextPage } from 'next'
+
+const meta = {
+  title: 'Blog',
+  description: `I've been writing online since 2020, mostly about data science, machine learning and tech careers. On purpose for documentation while able to share my knowledge. Use the search below to filter by title.`
+}
+
+interface BlogPageProps {
+  latestPost: Array<BlogProps & { slug: string }>
+  allPost: Array<BlogProps & { slug: string }>
+}
+
+const BlogPage: NextPage<BlogPageProps> = ({ latestPost = [], allPost = [] }) => {
+  const { query, handleChange, filteredBlog } = useSearchBlogQuery(latestPost)
 
   return (
     <Layout as='main' {...meta}>
       <Hero {...meta} />
+
+      <Searchbar onChange={handleChange} value={query} placeholder='Search Posts..' />
+
+      {query.length === 0 && <BlogList blogs={latestPost} title='Latest Post' className='mb-20' />}
+      {query.length === 0 && <BlogList blogs={allPost} title='All Post' layout='column' />}
+
+      {query.length > 0 && filteredBlog.length > 0 ? (
+        <BlogList blogs={filteredBlog} title='Search Result' layout='column' />
+      ) : null}
+
+      {query.length > 0 && filteredBlog.length === 0 ? <p>No result found</p> : null}
     </Layout>
   )
+}
+
+export const getStaticProps: GetStaticProps = () => {
+  const blogs = getBlog()
+
+  return {
+    props: {
+      latestPost: blogs
+        .map((b) => ({ ...b.data, slug: b.slug }))
+        .sort((a, b) =>
+          new Date(a.published) > new Date(a.published) ? 1 : new Date(a.published) < new Date(b.published) ? -1 : 0
+        ),
+      allPost: blogs.map((b) => ({ ...b.data, slug: b.slug }))
+    }
+  }
 }
 
 export default BlogPage
