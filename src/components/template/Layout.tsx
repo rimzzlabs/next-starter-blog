04f83623294fd39711f86ed1b31b8329b5de5eaf
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-04-19 21:23:37 +0700

    style: modify margin to y axis

--- src/components/template/Layout.tsx
+++ src/components/template/Layout.tsx
@@ -18,7 +18,7 @@ interface LayoutProps extends CustomSeoProps {
  * with a CustomSeo component and the children prop.
  */
 const Layout: React.FunctionComponent<LayoutProps> = ({ children, as = 'div', ...props }) => {
-  return React.createElement(as, { className: 'layout mt-20' }, <CustomSeo {...props} />, children)
+  return React.createElement(as, { className: 'layout my-20' }, <CustomSeo {...props} />, children)
 }
 
 export default Layout
