785374402163adc529ddf8a6c047f9110a7d1d7a
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-04-19 21:21:30 +0700

    feat: create BlogList component
    with 2 type of layout, column and row

---
+++ src/components/organism/BlogList.tsx
@@ -0,0 +1,33 @@
+import BlogCard from '@/components/mollecules/BlogCard'
+
+import { BlogProps } from '@/data/blog/blog.type'
+
+import clsx from 'clsx'
+
+interface BlogListProps {
+  title: string
+  blogs: Array<BlogProps & { slug: string }>
+  layout?: 'column' | 'row'
+  className?: string
+  children?: React.ReactNode
+}
+
+const BlogList: React.FunctionComponent<BlogListProps> = ({ blogs, children, title, className, layout = 'row' }) => {
+  return (
+    <section className={className}>
+      <h2>{title}</h2>
+      {blogs.length > 0 && (
+        <ul className={clsx('grid grid-cols-1 gap-4 flex-auto', layout === 'row' && 'md:grid-cols-2')}>
+          {blogs.map((val) => (
+            <li key={val.slug}>
+              <BlogCard {...val} />
+            </li>
+          ))}
+        </ul>
+      )}
+      {children}
+    </section>
+  )
+}
+
+export default BlogList
