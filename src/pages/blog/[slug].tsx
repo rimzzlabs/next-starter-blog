7ad75c4d6d309a570c0c5f81a4b28f6f69f39819
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-04-20 22:28:31 +0700

    feat: using useMetaData to get all meta data needed

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,7 +1,9 @@
+import CustomImage from '@/components/mollecules/CustomImage'
 import Layout from '@/components/template/Layout'
 
 import { BlogProps } from '@/data/blog/blog.type'
 import getBlog, { getBlogBySlug } from '@/helpers/getBlog'
+import useMetaData from '@/hooks/useMetaData'
 import { dateFormat } from '@/libs/dateFormat'
 
 import clsx from 'clsx'
@@ -17,20 +19,41 @@ interface URLSlug extends ParsedUrlQuery {
 
 interface BlogPostProps {
   mdxSource: MDXRemoteSerializeResult
-  data: BlogProps
+  data: BlogProps & { slug: string }
 }
 
 const BlogPost: NextPage<BlogPostProps> = ({ data, mdxSource }) => {
-  return (
-    <Layout as='article' title={data.title} description={data.summary}>
-      <section className='border-b max-w-prose border-main-2 dark:border-main-3 py-10'>
-        <h1>{data.title}</h1>
-        <p>Published on {dateFormat(data.published)}</p>
-      </section>
+  const metaData = useMetaData(data)
 
-      <section className={clsx('prose md:prose-lg dark:prose-invert py-20')}>
-        <MDXRemote {...mdxSource} />
-      </section>
+  return (
+    <Layout {...metaData} as='main' title={data.title} description={data.summary}>
+      <article>
+        <figure className='w-full pt-0'>
+          <CustomImage
+            src={data.thumbnail ?? '/static/default-thumbnail.jpg'}
+            alt={data.title}
+            display='intrinsic'
+            width={768}
+            height={376}
+            objectFit='cover'
+            className='rounded'
+          />
+        </figure>
+        <section className='border-b max-w-prose border-main-2 dark:border-main-3 py-10'>
+          <h1>{data.title}</h1>
+          <p className='text-sm'>Published on {dateFormat(data.published)}</p>
+          <p className='text-sm'>Written by {data.author_name}</p>
+        </section>
+
+        <section
+          className={clsx(
+            'prose md:prose-lg dark:prose-invert py-20',
+            'prose-a:no-underline prose-a:font-semibold prose-a:text-primary-4'
+          )}
+        >
+          <MDXRemote {...mdxSource} />
+        </section>
+      </article>
     </Layout>
   )
 }
@@ -58,7 +81,7 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
   return {
     props: {
       mdxSource,
-      data: blog.data
+      data: { ...blog.data, slug }
     }
   }
 }
