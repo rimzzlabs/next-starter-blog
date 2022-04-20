a3022547eeb6cc37f35feb75a8a4161a9fd019bf
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-04-20 22:53:57 +0700

    feat: create UnderlineLink component
    an anchor that has underline on hover

---
+++ src/components/mollecules/UnderlineLink.tsx
@@ -0,0 +1,27 @@
+import UnstyledLink, { UnstyledLinkProps } from '@/components/atoms/UnstyledLink'
+
+import clsx from 'clsx'
+
+export interface UnderlineLinkProps extends UnstyledLinkProps {
+  children: React.ReactNode
+}
+
+const UnderlineLink: React.FC<UnderlineLinkProps> = ({ children, ...props }) => {
+  return (
+    <UnstyledLink
+      className={clsx(
+        'relative border-b-2 border-dotted font-semibold',
+        'border-main-3 dark:border-main-2',
+        'after:absolute after:-bottom-0.5 after:left-0 hover:after:w-full',
+        'after:h-[3px] after:w-0 after:transition-all after:duration-300',
+        'after:bg-primary-3 dark:after:bg-primary-2',
+        'text-primary-4 dark:text-primary-2 hover:text-primary-5 dark:hover:text-primary-4'
+      )}
+      {...props}
+    >
+      {children}
+    </UnstyledLink>
+  )
+}
+
+export default UnderlineLink
