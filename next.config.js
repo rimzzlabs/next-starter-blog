632165a29afba56675014b0eef6f177979485f4d
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-04-20 22:31:46 +0700

    feat: add docs of how to use external images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true
+  // if you want to use external image, for example from unsplash, you need to set the images property and add to it the url of the image, uncomment the following line.
+  // more info: on https://nextjs.org/docs/basic-features/image-optimization#domains
+  // images: ['images.unsplash.com']
 }
 
 module.exports = nextConfig
