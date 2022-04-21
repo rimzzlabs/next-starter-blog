8792ad3f754323b96884765407e47c404b79f773
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-04-21 23:28:55 +0700

    style: using underline link component instead

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,8 @@
+import UnderlineLink from '@/components/mollecules/UnderlineLink'
 import Layout from '@/components/template/Layout'
 
 import clsx from 'clsx'
 import type { NextPage } from 'next'
-import Link from 'next/link'
 
 const NotFoundPage: NextPage = () => {
   const meta = {
@@ -14,13 +14,11 @@ const NotFoundPage: NextPage = () => {
     <Layout {...meta}>
       <section className={clsx('flex flex-col items-center justify-center', '-mt-16 md:-mt-20', 'min-h-screen')}>
         <h1 className='text-center'>404 | Not Found</h1>
-        <p className='text-center max-w-prose mt-4'>
+        <p className='text-center max-w-prose my-4'>
           The page you are looking for, are not found, if you find this was a mistake, please contact us at{' '}
           <em>company@mail.cs</em>
         </p>
-        <Link href='/'>
-          <a className={clsx('mt-4', 'text-primary-3 dark:text-primary-2')}>Back to Home</a>
-        </Link>
+        <UnderlineLink href='/'>Back To Home</UnderlineLink>
       </section>
     </Layout>
   )
