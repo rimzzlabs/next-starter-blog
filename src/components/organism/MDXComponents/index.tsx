5a25fbcdb42b2b024af440cd320f8aecacaef6c6
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-04-21 22:47:55 +0700

    feat: update MDXComponents
    added inline code and blockquote

--- src/components/organism/MDXComponents/index.tsx
+++ src/components/organism/MDXComponents/index.tsx
@@ -1,8 +1,12 @@
+import Blockquote from '@/components/contents/Blockquote'
+import InlineCode from '@/components/contents/InlineCode'
 import Pre from '@/components/contents/Pre'
 import UnderlineLink from '@/components/mollecules/UnderlineLink'
 
 const MDXComponents = {
   a: UnderlineLink,
+  blockquote: Blockquote,
+  code: InlineCode,
   pre: Pre
 }
 
