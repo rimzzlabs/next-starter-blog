95035e85c3506d5ba44d9865343954e73faf5c21
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-04-21 22:40:45 +0700

    style: add 100% height to the box and margin top auto to list of tags

--- src/components/mollecules/BlogCard.tsx
+++ src/components/mollecules/BlogCard.tsx
@@ -8,15 +8,15 @@ const BlogCard: React.FunctionComponent<BlogProps & { slug: string }> = ({ slug,
   return (
     <UnstyledLink
       className={clsx(
-        'inline-flex flex-col p-4 w-full',
+        'flex flex-col p-4 w-full h-full',
         'border rounded-lg transition',
         'border-main-2 dark:border-main-3',
         'hover:border-main-4 dark:hover:border-main-2'
       )}
       href={`/blog/${slug}`}
     >
-      <h3>{title}</h3>
-      <div className='mt-[0.675em] flex items-center space-x-2'>
+      <h3 className='mb-2'>{title}</h3>
+      <div className='flex items-center space-x-2 mt-auto'>
         {tags.map((t) => (
           <span key={t}>#{t.toLowerCase()}</span>
         ))}
