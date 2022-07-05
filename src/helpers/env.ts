42e19a207a08a86ead4dd617908667104e330678
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-07-05 09:53:04 +0700

    feat: create helpers to take environment variables and index file for re-exporting module

---
+++ src/helpers/env.ts
@@ -0,0 +1,2 @@
+export const ownerName = process.env.NEXT_PUBLIC_OWNER_NAME as string
+export const siteURL = process.env.NEXT_PUBLIC_SITE_URL as string
---
+++ src/helpers/index.ts
@@ -0,0 +1,7 @@
+export { getBlogBySlug } from './getBlog'
+export { default as getBlog } from './getBlog'
+
+export { CONTENT_DIRECTORY } from './getPathDirectory'
+export { default as getPathDirectory } from './getPathDirectory'
+
+export { ownerName, siteURL } from './env'
