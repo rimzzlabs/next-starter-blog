cd363f835102ecd9797804dc7120bbc84b9a7e88
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-07-05 09:56:52 +0700

    feat: solving module not found on nodejs module

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  // below resolve module not found on fs module
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false
+      }
+    }
+    return config
+  }
+  // above resolve module not found on fs module
   // if you want to use external image, for example from unsplash, you need to set the images property and add to it the url of the image, uncomment the following line.
   // more info: on https://nextjs.org/docs/basic-features/image-optimization#domains
   // images: ['images.unsplash.com']
